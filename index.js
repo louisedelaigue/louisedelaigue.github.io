59c1547313812ae62a996032dafe4b59c5da1cb5
Louise Delaigue <louise.delaigue@nioz.nl>
2020-12-01 20:48:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -65,3 +65,13 @@ $(document).ready(function(){
       });
   });
   
+var completes = document.querySelectorAll(".complete");
+var toggleButton = document.getElementById("toggleButton");
+
+
+function toggleComplete(){
+  var lastComplete = completes[completes.length - 1];
+  lastComplete.classList.toggle('complete');
+}
+
+toggleButton.onclick = toggleComplete;
